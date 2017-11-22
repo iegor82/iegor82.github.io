022f76eb5f94cd6c181b4184a2730d4b9ebfc135
Egor Ivanitsky <iegor@tut.by>
2017-11-22 14:26:09 +0300

    memory limit initial

---
+++ MemoryLimitTest/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    for (let i = 0; i < 800; ++i) {
+        var canvas = document.createElement("canvas");
+        canvas.width = 1000;
+        canvas.height = 1000;
+        let context = canvas.getContext("2d");
+        context.fillStyle = 'rgba(' + Math.random()*255 + ',' + Math.random()*255 + ',' + Math.random()*255 + ',1)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        $('.content').append(canvas);
+    }
+})
